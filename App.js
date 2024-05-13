b02b160715c6bd545883aad90c0b38c29a43f8e0
Rakshith Kakathkar <rakskakathkar@gmail.com>
2024-05-13 18:55:24 +0530

    configure parcel for bundling

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
-const heading = React.createElement("h1", {id: "heading"}, "This is h1 tag heading")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "This is h1 tag heading"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
