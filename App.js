ee04cff767e38bc401042c7116bdbdc5fc237a02
Rakshith Kakathkar <rakskakathkar@gmail.com>
2024-05-14 13:11:50 +0530

    different ways of component creation

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { class: "heading" },
-//   "This is h1 tag heading"
-// );
+// using React.createElement
+const heading = React.createElement(
+  "h1",
+  { class: "heading" },
+  "This is h1 tag heading"
+);
 
-const jsxHeading = <h1 className="heading">This is from JSX</h1>
+// creating a React element
+const jsxHeading = <h1 className="heading">This is from JSX</h1>;
+
+// creating a React component
+const HeadingComponent = () => (
+  <div>
+    {heading}
+    {jsxHeading}
+    <h1 className="heading">This is a functional component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
  
