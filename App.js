e2ee897d327d55a25628ac40de55d829f33f6ee3
Rakshith Kakathkar <rakskakathkar@gmail.com>
2024-05-14 12:25:35 +0530

    npm scripts added

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is h1 tag heading"
-);
+
+// const heading = React.createElement(
+//   "h1",
+//   { class: "heading" },
+//   "This is h1 tag heading"
+// );
+
+const jsxHeading = <h1 className="heading">This is from JSX</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
+ 
