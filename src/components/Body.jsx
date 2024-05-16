2b156b4b7aaf7c56df48f9ba36fe78ba7e948463
Rakshith Kakathkar <rakskakathkar@gmail.com>
2024-05-16 19:32:42 +0530

    filter top res - useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import { RES_LIST } from "../utils/mockData";
+import { useState } from "react";
 const Body = () => {
+  const [resList, setResList] = useState(RES_LIST);
+  const filterTopRes = () => {
+    setResList(RES_LIST.filter((res) => res.info.avgRating > 4));
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={filterTopRes}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {RES_LIST.map((res) => (
+        {resList?.map((res) => (
           <RestaurantCard resData={res} key={res.info.id} />
         ))}
       </div>
-    </div>
+    </div> 
   );
 };
 
